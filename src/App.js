c27891d45400f910730f862468ace42a0ffe1b33
Jeff Richardson <jjrich13@gmail.com>
2018-10-24 17:12:43 -0500

    Added firebase config

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 import reducers from './reducers'
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount(){
+        const config = {
+            apiKey: 'AIzaSyDru-nqdIjRL_DIB2fqiCgMduEQ_VyIMTI',
+            authDomain: 'manager-f2207.firebaseapp.com',
+            databaseURL: 'https://manager-f2207.firebaseio.com',
+            projectId: 'manager-f2207',
+            storageBucket: 'manager-f2207.appspot.com',
+            messagingSenderId: '47497737138'
+          };
+          firebase.initializeApp(config);
+    }
     render(){
         return(
             <Provider store={createStore(reducers)}>
