3164aa509a1663cc35c54f986670541b7bce6a98
Jeff Richardson <jjrich13@gmail.com>
2018-11-05 14:29:45 -0600

    added authReducer case to house user

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,4 +1,4 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types'
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from '../actions/types'
 
 const initialState = { email: '', password: '' }
 
@@ -7,9 +7,11 @@ export default (state = initialState, action) => {
     
     switch (action.type) {
         case EMAIL_CHANGED:
-            return {...state, email: action.payload}
+            return {...state, email: action.payload};
         case PASSWORD_CHANGED:
-            return {...state, password: action.payload}
+            return {...state, password: action.payload};
+        case LOGIN_USER_SUCCESS:
+            return { ...state, user: action.payload };
         default:
             return state;
     }
